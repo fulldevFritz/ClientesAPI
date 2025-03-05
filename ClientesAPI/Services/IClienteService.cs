467a119afb93c2376ca0935aa6b81a3011fe36f7
Felipe Fritz Pessoa <fritzlumen@hotmail.com>
2025-03-05 10:22:37 -0300

    Ajustes no git ignore e add Interface  Services Clientes

---
+++ ClientesAPI/Services/IClienteService.cs
@@ -0,0 +1,16 @@
+﻿using ClientesAPI.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ClientesAPI.Services
+{
+    public interface IClienteService
+    {
+        Task<IEnumerable<Cliente>> GetClientesAsync();
+        Task<IEnumerable<Cliente>> GetClientesByNomeAsync(string nome);
+        Task<Cliente> GetClienteAsync(int id);
+        Task<Cliente> CreateClienteAsync(Cliente cliente);
+        Task<Cliente> UpdateClienteAsync(int id, Cliente cliente);
+        Task<Cliente> DeleteClienteAsync(int id);
+    }
+}
